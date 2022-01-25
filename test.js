76138a76724fea739502662fc651062001ccfa23
Pierre TOP (TOPI) <ptop@octo.com>
2022-01-25 10:53:53 +0100

    forwrad

--- test.js
+++ test.js
@@ -1,5 +1,38 @@
 const { expect } = require('chai');
 
-it('fails', function() {
-  expect(1).to.equal(0);
+// test list
+// command :
+// - orientation l, r
+// - déplacement : f, b
+
+// scenari:
+// "ffrlff'
+// "f"
+// "ff"
+// "rr"
+// "l"
+// "f" mais d'un autre point de départ
+
+class Rover {
+  position ={
+    "x": 2,
+    "y": 3
+  }
+
+  evalCommands(f) {
+
+  }
+}
+
+it('should go forward when sequence is f', function() {
+  //
+  const rover = new Rover();
+
+  // when
+  rover.evalCommands('f');
+
+  //
+  const actualPosition = rover.position;
+  expect(actualPosition).to.deep.equal({x: 2, y: 3});
 });
+
