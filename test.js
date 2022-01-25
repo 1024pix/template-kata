0f88d89e5658e69ce1533d15bb8131e56f9f2a9a
sroccaserra <sroccaserra@yahoo.com>
2022-01-25 11:16:29 +0100

    Add two steps forward

--- test.js
+++ test.js
@@ -13,26 +13,65 @@ const { expect } = require('chai');
 // "l"
 // "f" mais d'un autre point de départ
 
+// le point de départ et la direction sont implicites. Comment on les explicite ?
+
+// y
+//
+// ^
+// |
+// 3>
+// |.
+// |.
+// 01---> x
+
 class Rover {
-  position ={
-    "x": 2,
-    "y": 3
-  }
-
-  evalCommands(f) {
-
+  evalCommands(commands) {
+    this.position = {
+      x: 1,
+      y: 3
+    }
+    this.position.x += commands.length;
   }
 }
 
+// y
+//
+// ^
+// |
+// 3.>
+// | .
+// | .
+// 0-2--> x
+
 it('should go forward when sequence is f', function() {
-  //
+  // given
   const rover = new Rover();
 
   // when
   rover.evalCommands('f');
 
-  //
+  // then
   const actualPosition = rover.position;
   expect(actualPosition).to.deep.equal({x: 2, y: 3});
 });
 
+// y
+//
+// ^
+// |
+// 3..>
+// |  .
+// |  .
+// 0--3-> x
+
+it('should go two steps forward when sequence is ff', function() {
+  // given
+  const rover = new Rover();
+
+  // when
+  rover.evalCommands('ff');
+
+  // then
+  const actualPosition = rover.position;
+  expect(actualPosition).to.deep.equal({x: 3, y: 3});
+});
