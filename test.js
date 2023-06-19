c57bcbd55bd3a5619f785a632c773acd1206d1dc
Simon Chareyron <schareyron@ippon.fr>
2023-06-19 14:58:28 +0200

    Add test list

--- test.js
+++ test.js
@@ -3,11 +3,19 @@ const expect = chai.expect;
 import sinonChai from 'sinon-chai';
 chai.use(sinonChai);
 
-describe('When a test is written',function() {
-  it('should fail', function() {
+// Lorsque je donne 3 je reçois Fizz
+// Lorsque je donne 5 je reçois Buzz
+// Lorsque je donne 15 je reçois FizzBuzz
+// Lorsque je donne 2 je reçois 2
+// Lorsque je donne undefined je reçois ''
+// Lorsque je donne 'a' je reçois une Error
+
+describe('FizzBuzz',function() {
+  it("given 3 should return 'Fizz'", function() {
     // given
 
     // when
+
 
     // then
     expect(1).to.equal(0);
